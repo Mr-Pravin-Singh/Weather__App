8b509b920c15ab3db2c5f72ea9a6d9d8b3c803d5
pravin <pravins3545@gmail.com>
2022-10-20 18:53:08 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react'
+import axios from 'axios'
+import ShowTemp from './ShowTemp'
 
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState({
+        description:"",
+    
+        feels_like: 0,
+        temp_max: 0,
+        temp_min: 0,  
+        humidity: 0,
+        sunrise: 0,
+        sunset: 0,
+        country:"",
+        speed: 0,
+      
+  });
+
+
+
+  const handleClick = ()=>{
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=69a6e546b9746840605e63c55be7ea0f`)
+    .then((response)=>{
+      console.log(response.data.sys.sunrise) 
+     
+
+      setData({
+        description: response.data.weather[0].description,
+        feels_like: ((response.data.main.feels_like)- 273.15).toFixed(2),
+        
+        temp_max: ((response.data.main.temp_max)-273.15).toFixed(2),
+        temp_min: ((response.data.main.temp_min) -273.15).toFixed(2),
+        humidity: response.data.main.humidity,
+        sunrise: getTime(response.data.sys.sunrise),
+        sunset: getTime(response.data.sys.sunset),
+        country: response.data.sys.country,
+        speed: response.data.wind.speed,
+        
+      })
+    })
+  }
+
+  const getTime = (stamp)=>{
+    const date = new Date(stamp*1000);
+    return date.toTimeString();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <div className='container text-center my-2'>
+          <h1 className='whitee'>Weather app in deg C</h1>
+          <input type="text" className='form' value={city} onChange={(el)=>{
+            setCity(el.target.value)
+          }} />
+          {/* <input type="text" className='form-control' /> */}
+          <button className='btn btn-primary mx-2' type='submit' onClick={handleClick}>get temp</button>
+
+          
+        </div>
+        <ShowTemp text={data}></ShowTemp>
+      </>
+  )
 }
 
-export default App;
+export default App
+
